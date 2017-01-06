319760c8493e01e6a8602fb89d597699724d004a
BabakSamani <babak.j.samani@gmail.com>
2017-01-05 22:07:13 -0600

    New codes are added for creating triangular stars

--- triangle.py
+++ triangle.py
@@ -19,3 +19,45 @@ for i in range (1, m):
         #k = k +1
 for i in range (0, m):
     print Matrix [i]
+
+# From this line below is Babak's code
+import sys
+
+print "Enter the number of rows: "
+n = raw_input()
+r = int(n)
+
+c = (2*r-1)
+
+matrix = [[' ' for j in range(0, c)] for i in range(0, r)]
+
+# if(r%2 == 0):
+l = c/2
+m = c/2
+matrix[0][(c/2)] = '*'
+i = 1
+p = c/2 - 1
+while(i < r):
+	l = l - 1
+	m = m + 1
+
+	for s in range(l, c):
+		if(p <= m and p >= l):
+			matrix[i][p] = '*'
+		p = p + 2
+
+	if(p%2 == 0):
+		p = 1
+	else:
+		p = 0
+	i += 1
+		
+			
+# for x in range(r):
+# 	print x, matrix[x]
+
+for x in range(0, r):
+	line = ""
+	for y in range(0, c):
+		line = line + matrix[x][y]
+	print line
